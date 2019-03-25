2bdffee262533deceae192c09f54b2894ef7821f
leonardo960 <rosso_x96@hotmail.it>
2019-03-25 17:25:28 +0100

    requirements.txt added and image removed

--- SLAMRobot.py
+++ SLAMRobot.py
@@ -83,39 +83,3 @@ class SLAMAgent:
     def load(self, name, lastRandomValue):
         self.model.load_weights(name)
         self.epsilon = lastRandomValue
-# if __name__ == "__main__":
-    # # initialize gym environment and the agent
-    # # env = gym.make('CartPole-v0')
-	# # env = nostroEnvironment.create()
-    # agent = SLAMAgent(60, 4) #60 (ampiezza 120 gradi totali ma un raggio sparato ogni 2 gradi) raggi sparati dagli occhi all'ambiente, 4 azioni: avanti, indietro, destra, sinistra
-    # # Iterate the game
-    # for e in range(episodes):
-        # # reset state in the beginning of each game
-        # state = env.reset() #sarebbero i 60 punti che tornano all'occhio del robot in posizione di spawn [(x1,y1,dist1),(x2,y2,dist2)...]
-        # # time_t represents each frame of the game
-        # # Our goal is to keep the pole upright as long as possible until score of 500
-        # # the more time_t the more score
-        # for time_t in range(500):
-            # # turn this on if you want to render
-            # # env.render()
-            # # Decide action
-            # action = agent.act(state)
-			
-            # # Advance the game to the next frame based on the action.
-            # # Reward is 1 for every frame the pole survived
-            # next_state, reward, done = env.step(action) #env.step torna una tupla (next_state, reward, done)
-			
-            # # Remember the previous state, action, reward, and done
-            # agent.remember(state, action, reward, next_state, done)
-            # # make next_state the new current state for the next frame.
-            # state = next_state
-            # # done becomes True when the game ends
-            # # ex) The agent drops the pole
-            # if done:
-                # # print the score and break out of the loop
-                # print("episode: {}/{}, score: {}"
-                      # .format(e, episodes, time_t))
-                # break
-        # # train the agent with the experience of the episode
-        # agent.replay(32)
-    # agent.save("pesi_agente_slam")
