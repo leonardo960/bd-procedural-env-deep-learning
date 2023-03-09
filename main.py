8048663910830f2a0eef2ea3878a0546b9709dc6
giodegas <6838625+giodegas@users.noreply.github.com>
2023-03-09 11:25:26 +0100

    fix: cleared Box width warnings and requirements versions

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ class Guizero:
         self.app = App(title="Procedural Environment Neuro-symbolic Agent", width=800, height=600)
         self.menu_box = Box(self.app, layout="grid", width=800, height=250)
         Box(self.menu_box, height=50, width=800, grid=[0, 0])
-        Box(self.menu_box, height=20, grid=[0, 2])
-        Box(self.menu_box, height=20, grid=[0, 4])
+        Box(self.menu_box, height=20, width=800, grid=[0, 2])
+        Box(self.menu_box, height=20, width=800, grid=[0, 4])
         PushButton(self.menu_box, command=self.sight_environments_cmd, text="Visualize environments generated",
                    width=30,
                    height=1, grid=[0, 1])
@@ -31,8 +31,8 @@ class Guizero:
         env_files = [f for f in listdir("./environments") if isfile(join("./environments", f))]
         self.listbox = ListBox(self.env_view_box, items=env_files, scrollbar=True, width=350, height=350, grid=[0, 1])
 
-        Box(self.env_view_box, height=40, grid=[0, 0])
-        view_button_box = Box(self.env_view_box, layout="grid", grid=[0, 2])
+        Box(self.env_view_box, height=40, width=50, grid=[0, 0])
+        view_button_box = Box(self.env_view_box, height=40, width=50, layout="grid", grid=[0, 2])
         PushButton(view_button_box, command=self.view_cmd, text="Visualize", grid=[0, 0])
         PushButton(view_button_box, command=self.view_env_back_cmd, text="Back", grid=[1, 0])
 
@@ -41,7 +41,7 @@ class Guizero:
         self.generation_env_box2 = Box(self.generation_env_box, visible=False, layout="grid", grid=[1, 1])
         self.generation_env_button = Box(self.generation_env_box, visible=False, layout="grid", grid=[0, 3])
 
-        Box(self.generation_env_box, height=40, grid=[0, 0])
+        Box(self.generation_env_box, height=40, width=50, grid=[0, 0])
         Text(self.generation_env_box1, text="Number of bedrooms:", grid=[0, 0], size=10, bg=self.light_green)
         self.sliderBR = Slider(self.generation_env_box1, horizontal=True, end=2, grid=[1, 0])
         self.sliderBR.__setattr__("bg", self.light_green)
@@ -51,7 +51,7 @@ class Guizero:
         Text(self.generation_env_box2, text="MAX cabinets:", grid=[0, 1], size=10, bg=self.light_green)
         self.sliderBR_W = Slider(self.generation_env_box2, horizontal=True, end=2, grid=[1, 1])
 
-        Box(self.generation_env_box1, height=40, grid=[0, 1])
+        Box(self.generation_env_box1, height=40, width=50, grid=[0, 1])
 
         Text(self.generation_env_box1, text="Number of bathrooms:", grid=[0, 2], size=10, bg=self.pink)
         self.sliderBAR = Slider(self.generation_env_box1, horizontal=True, end=2, grid=[1, 2])
@@ -63,7 +63,7 @@ class Guizero:
         Text(self.generation_env_box2, text="MAX sinks:", grid=[0, 4], size=10, bg=self.pink)
         self.sliderBAR_SI = Slider(self.generation_env_box2, horizontal=True, end=1, grid=[1, 4])
 
-        Box(self.generation_env_box1, height=40, grid=[0, 3])
+        Box(self.generation_env_box1, height=40, width=50, grid=[0, 3])
 
         Text(self.generation_env_box1, text="Number of kitchens:", grid=[0, 4], size=10, bg=self.yellow)
         self.sliderKI = Slider(self.generation_env_box1, horizontal=True, end=2, grid=[1, 4])
@@ -74,7 +74,7 @@ class Guizero:
         Text(self.generation_env_box2, text="MAX balconies:", grid=[0, 6], size=10, bg=self.yellow)
         self.sliderKI_D = Slider(self.generation_env_box2, horizontal=True, end=3, grid=[1, 6])
 
-        Box(self.generation_env_box1, height=40, grid=[0, 5])
+        Box(self.generation_env_box1, height=40, width=50, grid=[0, 5])
 
         Text(self.generation_env_box1, text="Number of halls:", grid=[0, 6], size=10, bg=self.orange)
         self.sliderHA = Slider(self.generation_env_box1, horizontal=True, start=1, end=1, grid=[1, 6])
@@ -87,7 +87,7 @@ class Guizero:
         Text(self.generation_env_box2, text="MAX shelves:", grid=[0, 9], size=10, bg=self.orange)
         self.sliderHA_CB = Slider(self.generation_env_box2, horizontal=True, end=2, grid=[1, 9])
 
-        Box(self.generation_env_box, height=40, grid=[0, 2])
+        Box(self.generation_env_box, height=40, width=50, grid=[0, 2])
 
         PushButton(self.generation_env_button, command=self.generate_cmd, text="Generate Environments", grid=[0, 0])
         PushButton(self.generation_env_button, command=self.view_generation_back_cmd, text="Back", grid=[1, 0])
@@ -97,7 +97,7 @@ class Guizero:
         self.listbox2 = ListBox(self.train_box_view, items=env_files, scrollbar=True, width=350, height=350,
                                 grid=[0, 1])
 
-        Box(self.train_box_view, height=40, grid=[0, 0])
+        Box(self.train_box_view, height=40, width=50, grid=[0, 0])
         view_button_box2 = Box(self.train_box_view, layout="grid", grid=[0, 2])
         #PushButton(view_button_box2, command=self.train_cmd, text="Train", grid=[0, 0])
         PushButton(view_button_box2, command=self.view_train_back_cmd, text="Back", grid=[1, 0])
